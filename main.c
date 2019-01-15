b1a94390e285c43b1e333ccaab7c8d8be692f578
Your Name <you@example.com>
2019-01-15 11:29:08 +0800

    构建基本框架

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
 int main(){
+    int qian=0;
+    printf("进入atm取钱系统\n");
 
-    printf("hello world!");
+    while(1){
+        printf("1---存钱");
+        printf("2---取钱");
+        printf("2---余额");
+        printf("2---退出");
+
+        int code;
+        scanf("%d",&code);
+        printf("请选择编号\n");
+
+    }
 
     return 0;
 
