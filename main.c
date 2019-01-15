222e22bfcc0830f580574948f770b9e75a440ed7
Your Name <you@example.com>
2019-01-15 11:43:01 +0800

    存钱功能的创建

--- main.c
+++ main.c
@@ -3,31 +3,39 @@
 int main()
 {
     int qian = 0;
-    printf("进入atm取钱系统\n");
+    printf("jinruquqianxitong\n");
 
     while (1)
     {
-        printf("1---存钱");
-        printf("2---取钱");
-        printf("2---余额");
-        printf("2---退出");
-        printf("请选择编号\n");
-       
+        printf("1---cunqian");
+        printf("2---quqian");
+        printf("2---yu e");
+        printf("2---tuichu");
+        printf("qingxuanzexiangyinggongnengbianhao\n");
+
         int code;
         scanf("%d", &code);
-        
 
-        if(code=1){
+        if (code = 1)
+        {
+            printf("qingshucunchujine,dianjihuichejixu\n");
+            int cun;
+            scanf("%d", &cun);
+            cun += qian;
+            printf("cunqianchenggong,dianjihuichejixu\n");
 
+            char x;
+            scanf("%d", &x);
+            scanf("%d", &x);
         }
-        if(code=2){
-
+        if (code = 2)
+        {
         }
-        if(code=3){
-
+        if (code = 3)
+        {
         }
-        if(code=4){
-            
+        if (code = 4)
+        {
         }
     }
 
