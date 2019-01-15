c897d1586c3ec5519cd8637600f0ade9fc9da811
Your Name <you@example.com>
2019-01-15 11:33:45 +0800

    构建基本框架的修改

--- main.c
+++ main.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
 
-int main(){
-    int qian=0;
+int main()
+{
+    int qian = 0;
     printf("进入atm取钱系统\n");
 
-    while(1){
+    while (1)
+    {
         printf("1---存钱");
         printf("2---取钱");
         printf("2---余额");
         printf("2---退出");
-
-        int code;
-        scanf("%d",&code);
         printf("请选择编号\n");
+       
+        int code;
+        scanf("%d", &code);
+        
 
+        if(code=1){
+
+        }
+        if(code=2){
+
+        }
+        if(code=3){
+
+        }
+        if(code=4){
+            
+        }
     }
 
     return 0;
-
 }
